8bfbeb1c9d607db922191d2cd7a4a160e59d5723
NILESH K <nileshkapde2696@gmail.com>
2021-08-25 00:12:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Basic';
+ public  title1 = 'Basic';
+
+ public address='udgir';
+
+
 }
